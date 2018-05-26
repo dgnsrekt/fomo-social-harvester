0215cb8f5f094d6ed2fa60e174ae72b15640e366
devtest <run2devtest@gmail.com>
2018-05-26 14:38:13 -0500

    repaced fetchpage with base.fetchpage

--- fomo_social_harvester/scraper/link_scraper.py
+++ fomo_social_harvester/scraper/link_scraper.py
@@ -12,31 +12,11 @@ from requests_html import HTMLSession
 from tenacity import *
 
 # LOCAL-APP
+from .base import fetch_page
 from .utils import is_valid_telegram_link, is_valid_twitter_link
 from .utils import scraper_exception_handler
 
 
-@scraper_exception_handler()
-def fetch_page(url):
-    '''Fetches html from url
-    :param str url: Url to request from.
-
-    :return: requests.html
-    '''
-    logger = structlog.getLogger()
-
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-
-    session = HTMLSession()
-    response = session.get(url, headers=headers, timeout=10)
-    log = logger.bind(url=url, status_code=response.status_code)
-
-    if response.status_code == 200:
-        log.debug('successfully connected')
-        return response.html
-    else:
-        log.debug('Connection Error...')
 def parse_maxpages():
     url = 'https://api.coinmarketcap.com/v2/global/'
     data = fetch_page(url)
