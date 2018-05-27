b37ad70ede3c5ec1fd6a946ea7216c18412ba65d
devtest <run2devtest@gmail.com>
2018-05-27 09:17:00 -0500

    update docstrings and logging

--- fomo_social_harvester/scraper/base.py
+++ fomo_social_harvester/scraper/base.py
@@ -8,23 +8,24 @@ from .utils import scraper_exception_handler
 
 
 @scraper_exception_handler()
-def fetch_page(url):
-    '''Fetches html from url
-    :param str url: Url to fetch.
+def fetch_page(url, header=None):
+    '''Fetches HTML from URL
+    :param str URL: URL to fetch.
 
     :return: requests.html or None
     '''
     logger = structlog.getLogger()
 
     headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
     session = HTMLSession()
     response = session.get(url, headers=headers, timeout=10)
     log = logger.bind(url=url, status_code=response.status_code)
 
     if response.status_code == 200:
-        log.debug('successfully connected')
+        log.debug('Successfully Connected...')
         return response.html
     else:
         log.debug('Connection Error...')
