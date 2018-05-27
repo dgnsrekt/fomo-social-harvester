f6a7f28bada04ef9017a9b7d15a3a24d05214212
devtest <run2devtest@gmail.com>
2018-05-27 09:42:59 -0500

    added custom telegram header

--- fomo_social_harvester/scraper/telegram.py
+++ fomo_social_harvester/scraper/telegram.py
@@ -14,7 +14,13 @@ def parse_member_count(row):
     sleep(5)
     name = row.get('name')
     telegram_link = row.get('link')
-    html = fetch_page(telegram_link)
+
+    telegram_header = {
+        'Accept': 'text/html, application/xhtml+xml, application/xml; q=0.9, image/webp,image/apng, */*; q=0.8',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    }
+
+    html = fetch_page(telegram_link, header=telegram_header)
     text = html.text.split('\n')
 
     for line in text:
@@ -27,7 +33,5 @@ def parse_member_count(row):
             print('.', end='', flush=True)
             if cleaned:
                 return {'name': name, 'members': cleaned}
-
-    print(telegram_link)
 
     return {'name': name, 'members': None}
