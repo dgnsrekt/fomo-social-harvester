c6abff722cea61248820f6d517a7f126b5cec983
devtest <run2devtest@gmail.com>
2018-05-27 09:52:32 -0500

    updated constains

--- fomo_social_harvester/constains.py
+++ fomo_social_harvester/constains.py
@@ -3,3 +3,10 @@ from pathlib import Path
 BASEPATH = Path(__file__).parent
 LOGPATH = BASEPATH / 'logs'
 DATAPATH = BASEPATH / 'data'
+BENCHPATH = BASEPATH / 'benchdata'
+
+BAD_TWITTER_LINKS = BASEPATH / 'bad_twitter_links.log'
+
+# DATAPATH.mkdir()
+# datepath = DATAPATH / '2014-2-29'
+# datepath.mkdir()
